3e1db5b8f6ab386fe6c2d8c04d5d375244ccc2d4
lijian <ljwl20061006@163.com>
2020-11-04 18:06:38 +0800

    1

--- vue-ssr/server/express.js
+++ vue-ssr/server/express.js
@@ -1,33 +1,37 @@
 const Vue = require('vue')
 const server = require('express')()
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+// const renderer = require('vue-server-renderer').createRenderer()
+
+const hostname = '127.0.0.1'
+const port = 3000
 
 server.get('*', (req, res) => {
   const app = new Vue({
     data: {
-      url: req.url
+      url: hostname+':'+port
     },
     template: `<div>访问的url是：{{url}}</div>`
   })
   const renderer = require('vue-server-renderer').createRenderer({
-    template: require('fs').readFileSync('./index.template.html', 'utf-8')
+    template: fs.readFileSync(path.join(__dirname, './index.template.html'), 'utf-8')
   })
   renderer.renderToString(app, (err, html) => {
     if (err) {
       res.status(500).end('Error')
       return
     }
-    res.end(`
-    <!DOCTYPE html>
-      <html lang="en">
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `)
+    res.end(`${html}`)
   })
 })
-const HOST = process.env.HOST || '0.0.0.0'
-const PORT = process.env.PORT || 3333
 
-server.listen(PORT, HOST, () => {
-  console.log(`server is listening on ${HOST}:${PORT}`)
+
+
+http.createServer(server).listen(port, hostname, () => {
+  console.log(`服务器运行在 http://${hostname}:${port}/`)
 })
+// server.listen(PORT, HOST, () => {
+//   console.log(`server is listening on ${HOST}:${PORT}`)
+// })
