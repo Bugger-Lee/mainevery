fe7a5354fca162e1612313c6c313d42ef2686317
lijian <ljwl20061006@163.com>
2020-11-27 19:24:58 +0800

    js

--- ts/test4.js
+++ ts/test4.js
@@ -19,3 +19,41 @@ function addNum(x, y) {
     return x + y;
 }
 console.log(addNum(6, 7));
+// 可选参数 默认参数
+function change(x1, y1) {
+    if (y1 === void 0) { y1 = 5; }
+    return x1 + y1;
+}
+console.log(change(8));
+function change1(x1, y1) {
+    if (y1) {
+        return x1 + y1;
+    }
+    else {
+        return x1;
+    }
+}
+console.log(change1(8), change1(8, 8));
+// 剩余参数
+function addString(x) {
+    var y = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        y[_i - 1] = arguments[_i];
+    }
+    console.log(y);
+    return x + y.join('');
+}
+console.log(addString('sa', 'sa', 'cy', '123'));
+function addNumber() {
+    var y = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        y[_i] = arguments[_i];
+    }
+    console.log(y);
+    var num = 0;
+    for (var i = 0; i < y.length; i++) {
+        num += y[i];
+        console.log(num);
+    }
+}
+addNumber(1, 2, 3, 4, 5, 6, 7, 8);
--- ts/test4.ts
+++ ts/test4.ts
@@ -21,3 +21,38 @@ function addNum(x: number, y: number) {
   return x + y
 }
 console.log(addNum(6, 7))
+
+// 可选参数 默认参数
+function change(x1: number, y1:number = 5) {
+  return x1+y1
+}
+console.log(change(8))
+
+function change1(x1: number, y1?:number) {
+  if(y1) {
+    return x1+y1
+  } else {
+    return x1
+  }
+}
+console.log(change1(8), change1(8, 8))
+
+// 剩余参数
+
+function addString(x:string, ...y: string[]) {
+  console.log(y)
+  return x + y.join('')
+}
+console.log(addString('sa', 'sa', 'cy', '123'))
+
+function addNumber(...y: number[]) {
+  console.log(y)
+  var num:number = 0
+  for (var i =0; i< y.length; i++) {
+    num += y[i]
+    console.log(num)
+  }
+}
+addNumber(1,2,3,4,5,6,7,8)
+
+// 匿名函数 自调用
