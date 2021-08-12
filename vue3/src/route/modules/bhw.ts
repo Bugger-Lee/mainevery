3be6e90aa85a761235a68715ccbd97018e0abde9
lijian <lijian@sikongtech.com>
2021-08-12 13:45:34 +0800

    百合网

--- vue3/src/route/modules/bhw.ts
+++ vue3/src/route/modules/bhw.ts
@@ -1,10 +1,18 @@
 import bhw from '../../views/Bhw/bhw.vue'
+import bhw1 from '../../views/Bhw/bhw1.vue'
 export default [
   {
     path: '/bhw',
     component: bhw,
     meta: {
       title: '约惠七夕 为爱免单'
+    }
+  },
+  {
+    path: '/bhw1',
+    component: bhw1,
+    meta: {
+      title: '鹊桥会-狼人杀活动'
     }
   }
 ]
